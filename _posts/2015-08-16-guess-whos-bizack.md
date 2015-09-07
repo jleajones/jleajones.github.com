15436fbfca3bce0d608f4919391de377e9e2cac7
Jason Lea-Jones <jleajones@gmail.com>
2015-09-07 15:28:45 +0400

    updates for new files

---
+++ _posts/2015-08-16-guess-whos-bizack.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Guess Who's Bizack"
+date: 2015-08-16 11:18:58
+category: music
+image: studio.png
+---
+Decided to step back into my home studio to make some heat!
---
+++ assets/js/page.js
@@ -0,0 +1,11 @@
+var page = (function() {
+    var page = {};
+    
+    //tracking
+    page.tracker = new Tracker({
+        
+    });
+    page.tracker.track();
+    
+    return page;
+}());
